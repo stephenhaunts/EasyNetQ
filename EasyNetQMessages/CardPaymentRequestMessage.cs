3cfa8585be9e7a8f0e3bccd16cc0164e5fc950b5
stephen haunts <stephen.haunts@outlook.com>
2018-07-10 19:59:34 +0100

    Add sender application for module 3 that sends 4 payments to RabbitMQ

---
+++ EasyNetQMessages/CardPaymentRequestMessage.cs
@@ -0,0 +1,10 @@
+﻿namespace EasyNetQMessages
+{
+    public class CardPaymentRequestMessage
+    {
+        public string CardNumber { get; set; }
+        public string CardHolderName { get; set; }
+        public string ExpiryDate { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
