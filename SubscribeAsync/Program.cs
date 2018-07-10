b3262d6f88f75f052fa981a094fbe6308d98bf8e
stephen haunts <stephen.haunts@outlook.com>
2018-07-10 20:31:52 +0100

    Add synchronous and async subscribe example apps to module 3.

--- SubscribeAsync/Program.cs
+++ SubscribeAsync/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Threading.Tasks;
+using EasyNetQ;
+using EasyNetQMessages;
 
 namespace SubscribeAsync
 {
@@ -6,7 +9,32 @@ namespace SubscribeAsync
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using (var bus = RabbitHutch.CreateBus("host=localhost"))
+            {
+                bus.SubscribeAsync<CardPaymentRequestMessage>("cardPayment", message => Task.Factory.StartNew(() =>
+                {
+                    Console.WriteLine("Processing Payment = <" + 
+                                      message.CardNumber + ", " + 
+                                      message.CardHolderName + ", " + 
+                                      message.ExpiryDate + ", " + 
+                                      message.Amount + ">");
+                }).ContinueWith(task =>
+                {
+                    if (task.IsCompleted && !task.IsFaulted)
+                    {
+                        Console.WriteLine("Finished processing all messages");
+                    }
+                    else
+                    {
+                        // Dont catch this, it is caught further up the heirarchy and results in being sent to the default error queue
+                        // on the broker
+                        throw new EasyNetQException("Message processing exception - look in the default error queue (broker)");
+                    }
+                }));
+
+                Console.WriteLine("Listening for messages. Hit <return> to quit.");
+                Console.ReadLine();
+            }
         }
     }
 }
