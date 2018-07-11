f92d905a37b476c258baffa35ed448ed556e59bf
stephen haunts <stephen.haunts@outlook.com>
2018-07-11 11:45:50 +0100

    Add empty projects for the named queues examples for module 3

---
+++ NamedQueueSubscribe/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NamedQueueSubscribe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
