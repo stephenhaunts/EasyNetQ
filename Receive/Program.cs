28530dc74fe634d9ea90578926860537bc623703
stephen haunts <stephen.haunts@outlook.com>
2018-07-12 11:36:31 +0100

    Add code for standard send and receive

--- Receive/Program.cs
+++ Receive/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using EasyNetQ;
+using EasyNetQMessages;
 
 namespace Receive
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using (var bus = RabbitHutch.CreateBus("host=localhost"))
+            {
+                bus.Receive<CardPaymentRequestMessage>("my.paymentsqueue", message => HandleCardPaymentMessage(message));
+
+                Console.WriteLine("Listening for messages. Hit <return> to quit.");
+                Console.ReadLine();
+            }
+        }
+
+        static void HandleCardPaymentMessage(CardPaymentRequestMessage paymentMessage)
+        {
+            Console.WriteLine("Processing Payment = <" +
+                              paymentMessage.CardNumber + ", " +
+                              paymentMessage.CardHolderName + ", " +
+                              paymentMessage.ExpiryDate + ", " +
+                              paymentMessage.Amount + ">");
         }
     }
 }
--- Send/Program.cs
+++ Send/Program.cs
@@ -1,12 +1,53 @@
 ﻿using System;
+using EasyNetQ;
+using EasyNetQMessages;
 
 namespace Send
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var payment1 = new CardPaymentRequestMessage
+            {
+                CardNumber = "1234123412341234",
+                CardHolderName = "Mr F Bloggs",
+                ExpiryDate = "12/12",
+                Amount = 99.00m
+            };
+
+            var payment2 = new CardPaymentRequestMessage
+            {
+                CardNumber = "3456345634563456",
+                CardHolderName = "Mr S Claws",
+                ExpiryDate = "03/11",
+                Amount = 15.00m
+            };
+
+            var payment3 = new CardPaymentRequestMessage
+            {
+                CardNumber = "6789678967896789",
+                CardHolderName = "Mrs E Curry",
+                ExpiryDate = "01/03",
+                Amount = 1250.24m
+            };
+
+            var payment4 = new CardPaymentRequestMessage
+            {
+                CardNumber = "9991999299939994",
+                CardHolderName = "Mrs D Parton",
+                ExpiryDate = "04/07",
+                Amount = 34.87m
+            };
+
+            using (var bus = RabbitHutch.CreateBus("host=localhost"))
+            {
+                bus.Send("my.paymentsqueue", payment1);
+                bus.Send("my.paymentsqueue", payment2);
+                bus.Send("my.paymentsqueue", payment3);
+                bus.Send("my.paymentsqueue", payment4);
+            }
+          
         }
     }
 }
