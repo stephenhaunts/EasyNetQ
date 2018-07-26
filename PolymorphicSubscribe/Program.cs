c0bf7f62e36e533e2d02e468ce117a33f4711bb6
stephen haunts <stephen.haunts@outlook.com>
2018-07-26 14:26:43 +0100

    Little tweak to the polymorphic code to make it more readable.

--- PolymorphicSubscribe/Program.cs
+++ PolymorphicSubscribe/Program.cs
@@ -9,36 +9,38 @@ namespace PolymorphicSubscribe
         static void Main(string[] args)
         {
             using (var bus = RabbitHutch.CreateBus("host=localhost"))
-            { 
-                bus.Subscribe<IPayment>("payments", @interface =>
-                {
-                    var cardPayment = @interface as CardPayment;
-                    var purchaseOrder = @interface as PurchaseOrder;
-
-                    if (cardPayment != null)
-                    {
-                        Console.WriteLine("Processing Card Payment = <" +
-                                          cardPayment.CardNumber + ", " +
-                                          cardPayment.CardHolderName + ", " +
-                                          cardPayment.ExpiryDate + ", " +
-                                          cardPayment.Amount + ">");
-                    }
-                    else if (purchaseOrder != null)
-                    {
-                        Console.WriteLine("Processing Purchase Order = <" +
-                                          purchaseOrder.CompanyName + ", " +
-                                          purchaseOrder.PoNumber + ", " +
-                                          purchaseOrder.PaymentDayTerms + ", " +
-                                          purchaseOrder.Amount + ">");
-                    }
-                    else
-                    {
-                        Console.Out.WriteLine("Invalid message. Needs to be a Card Payment or Purchase Order.");
-                    }
-                });
+            {
+                bus.Subscribe<IPayment>("payments", message => HandleMessage(message));
 
                 Console.WriteLine("Listening for messages. Hit <return> to quit.");
                 Console.ReadLine();
+            }
+        }
+
+        private static void HandleMessage(IPayment message)
+        {
+            var cardPayment = message as CardPayment;
+            var purchaseOrder = message as PurchaseOrder;
+
+            if (cardPayment != null)
+            {
+                Console.WriteLine("Processing Card Payment = <" +
+                                  cardPayment.CardNumber + ", " +
+                                  cardPayment.CardHolderName + ", " +
+                                  cardPayment.ExpiryDate + ", " +
+                                  cardPayment.Amount + ">");
+            }
+            else if (purchaseOrder != null)
+            {
+                Console.WriteLine("Processing Purchase Order = <" +
+                                  purchaseOrder.CompanyName + ", " +
+                                  purchaseOrder.PoNumber + ", " +
+                                  purchaseOrder.PaymentDayTerms + ", " +
+                                  purchaseOrder.Amount + ">");
+            }
+            else
+            {
+                Console.Out.WriteLine("Invalid message. Needs to be a Card Payment or Purchase Order.");
             }
         }
     }
